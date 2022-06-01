ae707b4a21244269dcbf45118414aec013fa7310
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-06-01 17:21:27 +0530

    Update telegraph.py

--- plugins/josprojects/telegraph.py
+++ plugins/josprojects/telegraph.py
@@ -1,68 +1,80 @@
-#!/usr/bin/env python3
-# Copyright (C) @ZauteKm
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+from events import register
+from telethn as tbot
+TMP_DOWNLOAD_DIRECTORY = "./"
+from telethon import events, Button, custom
 import os
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from telegraph import upload_file
+from PIL import Image
+from datetime import datetime
+from telegraph import Telegraph, upload_file, exceptions
+alive = "ALIVE"
+telegraph = Telegraph()
+r = telegraph.create_account(short_name=alive)
+auth_url = r["auth_url"]
 
-@Client.on_message(filters.command(["tgmedia", "tgraph", "telegraph"]))
-async def telegraph(client, message):
-    replied = message.reply_to_message
-    if not replied:
-        await message.reply("Reply to a supported media file")
+@register(pattern="^/t(m|xt) ?(.*)")
+async def _(event):
+    if event.fwd_from:
         return
-    if not (
-        (replied.photo and replied.photo.file_size <= 5242880)
-        or (replied.animation and replied.animation.file_size <= 5242880)
-        or (
-            replied.video
-            and replied.video.file_name.endswith(".mp4")
-            and replied.video.file_size <= 5242880
-        )
-        or (
-            replied.document
-            and replied.document.file_name.endswith(
-                (".jpg", ".jpeg", ".png", ".gif", ".mp4"),
+    optional_title = event.pattern_match.group(2)
+    if event.reply_to_msg_id:
+        start = datetime.now()
+        r_message = await event.get_reply_message()
+        input_str = event.pattern_match.group(1)
+        if input_str == "m":
+            downloaded_file_name = await tbot.download_media(
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
             )
-            and replied.document.file_size <= 5242880
-        )
-    ):
-        await message.reply("Not supported!")
-        return
-    download_location = await client.download_media(
-        message=message.reply_to_message,
-        file_name="root/downloads/",
-    )
-    try:
-        response = upload_file(download_location)
-    except Exception as document:
-        await message.reply(message, text=document)
+            end = datetime.now()
+            ms = (end - start).seconds
+            h = await event.reply("Downloaded {} in {} seconds.".format(downloaded_file_name, ms))
+            if downloaded_file_name.endswith((".webp")):
+                resize_image(downloaded_file_name)
+            try:
+                start = datetime.now()
+                media_urls = upload_file(downloaded_file_name)
+            except exceptions.TelegraphException as exc:
+                await h.edit("ERROR: " + str(exc))
+                os.remove(downloaded_file_name)
+            else:
+                end = datetime.now()
+                BUTTON = [[Button.url("Telegraph", f"https://telegra.ph/{media_urls[0]}")]]
+                ms_two = (end - start).seconds
+                os.remove(downloaded_file_name)
+                await h.delete()
+                await event.reply("Uploaded to [Telegraph](https://telegra.ph{})".format(media_urls[0]), link_preview=True, buttons=BUTTON)
+        elif input_str == "xt":
+            user_object = await tbot.get_entity(r_message.sender_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
+            # apparently, all Users do not have last_name field
+            if optional_title:
+                title_of_page = optional_title
+            page_content = r_message.message
+            if r_message.media:
+                if page_content != "":
+                    title_of_page = page_content
+                downloaded_file_name = await tbot.download_media(
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
+                )
+                m_list = None
+                with open(downloaded_file_name, "rb") as fd:
+                    m_list = fd.readlines()
+                for m in m_list:
+                    page_content += m.decode("UTF-8") + "\n"
+                os.remove(downloaded_file_name)
+            page_content = page_content.replace("\n", "<br>")
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
+            )
+            end = datetime.now()
+            ms = (end - start).seconds
+            await event.reply("Pasted to [Telegraph](https://telegra.ph/{}) in {} seconds.".format(response["path"], ms), link_preview=True, buttons=BUTTON)
     else:
-        await message.reply(
-            f"<b>Link:-</b>\n\n <code>https://telegra.ph{response[0]}</code>",
-            quote=True,
-            reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(text="open link", url=f"https://telegra.ph{response[0]}"),
-                    InlineKeyboardButton(text="share link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
-                ],
-                [InlineKeyboardButton(text="✗ Close ✗", callback_data="close_data")]
-            ]
-        )
-    )
-    finally:
-        os.remove(download_location)
+        await event.reply("Reply to a message to get a permanent telegra.ph link.")
+
+
+def resize_image(image):
+    im = Image.open(image)
+    im.save(image, "PNG")
