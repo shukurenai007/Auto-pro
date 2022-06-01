89cb696faff69169af5a959d6ad81c40c783b478
SHU KURENAI TEAM <94837807+shukurenai007@users.noreply.github.com>
2022-06-01 18:38:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/shukurenai007/auto-pro.git /IMDb-Movie-Bot
-cd /IMDb-Movie-Bot
+git clone -b master https://github.com/shukurenai007/auto-pro.git /auto-pro
+cd /auto-pro
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
